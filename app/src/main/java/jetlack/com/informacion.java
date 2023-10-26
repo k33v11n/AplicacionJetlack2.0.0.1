fe9768950c72029b8caa9a58754f265ecbb1c2a5
sistemas <kevinhurtado781@gmail.com>
2023-10-26 16:05:44 -0300

    Initial commit

---
+++ app/src/main/java/jetlack/com/informacion.java
@@ -0,0 +1,14 @@
+package jetlack.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class informacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacion);
+    }
+}
